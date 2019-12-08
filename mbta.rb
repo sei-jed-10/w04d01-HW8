e067e8689fe88c985291021a2107f131398f7e41
Rooz <razan@gmail.com>
2019-12-08 20:50:06 +0300

    hw8

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,26 @@
+REDLINE = ["south station", "park street", "kendall", "central", "harvard", "porter", "davis", "alewife"] 
+GREENLINE = ["government center", "park street", "boylston", "arlington", "copley", "hynes", "kenmore"]
+ORANGELINE = ["north station", "haymarket", "park street", "state", "downtown crossing", "chinatown",  "back bay", "forest hills"]
 
-# your solution here
-def stops_between_stations
 
-end
+def stops_between_stations(startLine, startStation, endLine, endStation)
+    if (startLine == "Red" and endLine == "Red")
+        p (REDLINE.index(startStation.downcase) - REDLINE.index(endStation.downcase)).abs
+    elsif (startLine == "Green" and endLine == "Green")
+        p (GREENLINE.index(startStation.downcase) - GREENLINE.index(endStation.downcase)).abs
+        elsif (startLine == "Orange" and endLine == "Orange")
+            p (ORANGELINE.index(startStation.downcase) - ORANGELINE.index(endStation.downcase)).abs
+        elsif ((startLine == "Red" and endLine == "Green") or (startLine == "Green" and endLine == "Red"))
+            p (REDLINE.index(startStation.downcase) - GREENLINE.index(endStation.downcase)).abs
+        elsif((startLine == "Red" and endLine == "Orange") or (startLine == "Orange" and endLine == "Red"))
+            p (REDLINE.index(startStation.downcase) - ORANGELINE.index(endStation.downcase)).abs
+
+        end
+    end
+
+    stops_between_stations('Red', 'Alewife', 'Red', 'Park Street')) // 3 stops
+stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing') // 5 stops
+stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') // 7 stops
+  
+
+
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,10 +1,15 @@
 #### Your Name:
-
+Razn Sannan
 ---
 
-##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)
+##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult) 
+5
 ##### How much of this did you complete? (0-10)
+10
 ##### How long did it take?
+3 hours
 ##### Was there anything that you struggled with?  If so, what?
-##### Is there anything that you would like some further information on?
+change from js to Ruby
+##### Is there anything that you would like some further information on? 
+no
 ##### Do you have any suggestions to improve this assignment?

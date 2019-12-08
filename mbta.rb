033697ac85c5fdfe572984aa7d84977c489aa070
Hanin Nouh <haninnouh@gmail.com>
2019-12-08 23:33:16 +0300

    HW8-Hanin Nouh

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,61 @@
 
-# your solution here
-def stops_between_stations
+subwayLines = [
+    Red= [
+      "South Station",
+      "Park Street",
+      "Kendall",
+      "Central",
+      "Harvard",
+      "Porter",
+      "Davis",
+      "Alewife"
+    ],
+    Green= [
+      "Government Center",
+      "Park Street",
+      "Boylston",
+      "Arlington",
+      "Copley",
+      "Hynes",
+      "Kenmore"
+    ],
+    Orange= [
+      "North Station",
+      "Haymarket",
+      "Park Street",
+      "State",
+      "Downtown Crossing",
+      "Chinatown",
+      "Back Bay",
+      "Forest Hills"
+    ]
+  ]
+  
+  def stopsBetweenStations(startLine, startStation, endLine, endStation)
 
-end
+     $startStationIndex = (startLine).index(startStation)
+
+     $endStationIndex = (endLine).index(endStation)
+
+     if (startLine === endLine) 
+      return ($startStationIndex - $endStationIndex).abs
+    end
+
+    $startLineParkStreetIndex = (startLine).index("Park Street")
+
+    $tripToParkStreet = ($startStationIndex - $startLineParkStreetIndex).abs
+
+    $endLineParkStreetIndex = (endLine).index("Park Street")
+
+    $tripToDestination = ($endStationIndex - $endLineParkStreetIndex).abs
+
+    $totalTrip = $tripToParkStreet + $tripToDestination
+
+    return $totalTrip
+
+  end
+
+  
+puts stopsBetweenStations(Red, 'Alewife', Red, 'Alewife') 
+puts stopsBetweenStations(Red, 'Alewife', Red, 'South Station')  
+puts (stopsBetweenStations(Red, 'Central', Green, 'Arlington'))

6491ac7774bb01e34ef16104c359f8e2a659c6c6
SafwanAlqulayti <57276846+SafwanAlqulayti@users.noreply.github.com>
2019-12-08 23:18:08 +0300

    Safwan Alqulayti
    Solution

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,63 @@
 
-# your solution here
-def stops_between_stations
+
+ 
+ $stations = {
+  "Red" =>["South Station" ,
+"Kendall" ,
+"Central",
+"Harvard",
+"Park Street",
+"Porter" ,
+"Davis" ,
+"Alewife"],
+ 
+"Green" =>[
+  "Government Center",
+"Boylston" ,
+"Arlington" ,
+"Park Street" ,
+"Copley" ,
+"Hynes",
+"Kenmore"
+],
+"Orange" => [
+ "North Station",
+"Haymarket" ,
+"State" ,
+"Downtown Crossing",
+"Chinatown" ,
+"Park Street" ,
+"Back Bay" ,
+"Forest Hills"
+]
+}
+
+ 
+ 
+ def calcultes(startline , startstation, endline , endstation)
+
+startstationindex = $stations[startline].index(startstation)
+
+endstationindex = $stations[endline].index(endstation)
+if (startline===endline)
+  return (startstationindex-endstationindex).abs
+end
+
+
+start_station_parkstrete = $stations[startline].index("Park Street")
+
+to_parkstreat = (startstationindex -start_station_parkstrete ).abs
+
+end_station_parkstrete = $stations[endline].index("Park Street")
+
+from_parkstret = (endstationindex - end_station_parkstrete).abs
+
+total_stops = (to_parkstreat-from_parkstret).abs
+return total_stops
 
 end
+ 
+
+ p calcultes("Red","Alewife","Red","Harvard")
+
+ 

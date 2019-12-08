8d398e6dfb92c4cfff647e722a12ab0427d67601
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2019-12-08 23:35:06 +0300

    Hatim Alattas
    ##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)
    5
    ##### How much of this did you complete? (0-10)
    10
    ##### How long did it take?
    2 hours
    ##### Was there anything that you struggled with?  If so, what?
    syntax
    ##### Is there anything that you would like some further information on?
    nope
    ##### Do you have any suggestions to improve this assignment?
    nope

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,22 @@
-
 # your solution here
-def stops_between_stations
+def stops_between_stations(startLine, startStation, endLine, endStation)
 
+    redLine = ["South Station", "Kendall", "Central", "Harvard", "Park Street", "Porter", "Davis", "Alewife"]
+    greenLine = ["Government Center", "Boylston", "Arlington", "Park Street", "Copley", "Hynes", "Kenmore"]
+    orangeLine = ["North Station", "Haymarket", "State", "Downtown Crossing", "Chinatown", "Park Street", "Back Bay", "Forest Hills"]
+    
+    if startLine === "Red" && endLine === "Green"
+        ((redLine.index(startStation) - redLine.index("Park Street")).abs + (greenLine.index(endStation) - greenLine.index("Park Street")).abs).abs
+
+    elsif startLine === "Red" && endLine === "Orange"
+        ((redLine.index(startStation) - redLine.index("Park Street")).abs + (orangeLine.index(endStation) - orangeLine.index("Park Street")).abs).abs
+
+    elsif startLine === "Red" && endLine === "Red"
+        (redLine.index(startStation) - redLine.index(endStation)).abs 
+    end
 end
+
+p stops_between_stations('Red', 'Alewife', 'Red', 'Park Street')
+p stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing')
+p stops_between_stations('Red', 'South Station', 'Green', 'Kenmore')
+

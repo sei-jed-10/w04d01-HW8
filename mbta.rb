fba53a310d5375bb62c7d3a80f82492b06f52ab4
Mohammedalamry <Mohammedalamry@users.noreply.github.com>
2019-12-09 08:28:53 +0300

    HW8 itis not complete

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,26 @@
 
 # your solution here
-def stops_between_stations
+def stops_between_stations initialize(Line1 , Station1, Line2,Station2)
+
+    @cust_id = id
+    @cust_name = name
+    @cust_addr = addr
+
+
+
+
+
 
 end
+
+
+
+
+
+
+
+
+
+stops_between_stations('Red', 'Alewife', 'Red', 'Park Street')) // 3 stops
+stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing') // 5 stops
+stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') // 7 stops

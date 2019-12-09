1d8239310f251b9c104200b4ab2bea9fd31308bc
Abdulrahman Alsulami <a.sulami96@gmail.com>
2019-12-09 11:30:57 +0300

    HW8 done

--- mbta.rb
+++ mbta.rb
@@ -1,47 +1,30 @@
-class Stations
-    
 
 
-    def initialize(start_line, strStation, end_line, enStation)
-        @start_line = start_line
-        @start_station = strStation
-        @end_line = end_line
-        @end_station = enStation
-     end
+def stops_between_stations(start_line,start_station,end_line,end_station)
 
-def stops_between_stations()
+subway={
+    red:["South Station","Kendall","Central","Harvard","Park Street","Porter","Davis","Alewife"],
+    green:["Government Center","Boylston","Arlington","Park Street","Copley","Hynes","Kenmore"],  
+    orange: ["North Station","Haymarket","State","Downtown Crossing","Chinatown","Park Street","Back Bay","Forest Hills"]
+}
 
+ str_station = subway[start_line.downcase.to_sym].index(start_station);
+ en_station = subway[end_line.downcase.to_sym].index(end_station);
+ break_line1 = subway[start_line.downcase.to_sym].index("Park Street")
+ break_line2 = subway[end_line.downcase.to_sym].index("Park Street")
+ first_stop = 0
+ second_stop = 0
 
-    red = Stations.new["South Station","Kendall","Central","Harvard","Park Street","Porter","Davis","Alewife"]
-    green = Stations.new["Government Center","Boylston","Arlington","Park Street","Copley","Hynes","Kenmore"]  
-    orange = Stations.new["North Station","Haymarket","State","Downtown Crossing","Chinatown","Park Street","Back Bay","Forest Hills"]
-
-
- strStation = @start_line.downcase.find_index(@start_station);
- enStation = @end_line.downcase.find_index(@end_station);
- br1 = start_line.downcase.find_index("Park Street")
- br2 = end_line.downcase.find_index("Park Street")
- firstStop = 0
- result = 0
-
-if start_line.downcase == end_line.downcase
-  result = (strStation - enStation).abs
- elsif start_line != end_line 
-    firstStop = (strStation - br1).abs
-    firstStop = ((enStation - br2) + firstStop).abs
-
-  return result
-
-  puts result
-
-
+if start_line == end_line
+  result = (str_station - en_station).abs
+else
+    first_stop = (str_station - break_line1).abs
+    second_stop = (((en_station - break_line2).abs)+ first_stop)
 
  end
  
 end
-stops_between_stations('Red', 'Alewife', 'Red', 'Park Street') 
-stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing') 
-stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') 
-class1obj = Stations.new
-class1obj.stops_between_stations
-end
+p stops_between_stations('Red', 'Alewife', 'Red', 'Park Street') 
+p stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing') 
+p stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') 
+

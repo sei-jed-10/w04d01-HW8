844f2e74084accbe5c1752c6cc803638973cd571
Bedo-11 <43325009+Bedo-11@users.noreply.github.com>
2019-12-09 08:04:20 +0300

    Update mbta.rb

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,26 @@
+subway = {
+    "red" => ["South Station", "Park Street" ,"Kendall" ,"Central" ,"Harvard" ,"Porter" ,"Davis" ,"Alewife"],
+    "green" => ["Government" ,"Center" ,"Park Street" ,"Boylston" ,"Arlington" ,"Copley" ,"Hynes" ,"Kenmore"],
+    "orange" =>  ["North Station" ,"Haymarket" ,"Park Street" ,"State" ,"Downtown Crossing" ,"Chinatown" ,"Back Bay" ,"Forest Hills"],
+    stops_between_stations: def stops_between_stations(startline,startstation,endline,endstation)
+        $steps = 0
+        startstation = startline.to_sym
+        # p startstation.index(startstation)
+        endstation = endstation.to_sym
+        # p endstation.index(endstation)
+        breakPointS = startstation.index('Park Street')
+        breakPointE = endline.index('Park Street')
+        $result =0
 
-# your solution here
-def stops_between_stations
+        if (startline == endline)
+            $result = (startstation - endstation).abs  
+        elsif (startline != endline)
+            $steps = (startstation - breakPointS).abs
+            $result = (endstation  - breakPointE).abs - steps 
+        end
+        return result
+    end 
+}
 
-end
+stops_between_stations('green', 'Alewife', 'Red', 'Park Street')
+

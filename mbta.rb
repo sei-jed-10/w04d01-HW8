f4fd7b1cc3bb89b322d8ff18797d237e64d8dc26
Bedo-11 <43325009+Bedo-11@users.noreply.github.com>
2019-12-09 10:41:28 +0300

    Update mbta.rb

--- mbta.rb
+++ mbta.rb
@@ -1,20 +1,49 @@
-def stops_between_stations (startline,startstation,endline,endstation)
-   subway = {
-    red:["South Station","Kendall","Central","Harvard", "Park Street", "Porter","Davis", "Alewife"],
-    green:["Government Center","Boylston","Arlington","Park Street", "Copley", "Hynes","Kenmore"],
-    orange:["North Station","Haymarket", "State", "Downtown Crossing", "Chinatown","Park Street", "Back Bay", "Forest Hills"]
-   }
-      startstation = subway[startline.to_sym].index(startstation)
-        endstation = subway[endline.to_sym].index(endstation)
-        breakPointS = subway[startline.to_sym].index("Park Street")
-        breakPointE = subway[endline.to_sym].index("Park Street")
-        if startline == endline
-            $result = (startstation - endstation).abs  
-        elsif startline != endline
-            steps = (startstation - breakPointS).abs
-            result = (endstation  - breakPointE).abs + steps 
-        end
-  end 
- p stops_between_stations('red', 'Alewife', 'red', 'Alewife') 
- p stops_between_stations('red', 'Alewife', 'red', 'South Station') 
-p stops_between_stations('red', 'South Station', 'orange', 'Forest Hills') 
+def stops_between_stations (start_line,start_station,end_line,end_station)
+    subway = {
+     red:["South Station","Kendall","Central","Harvard", "Park Street", "Porter","Davis", "Alewife"],
+     green:["Government Center","Boylston","Arlington","Park Street", "Copley", "Hynes","Kenmore"],
+     orange:["North Station","Haymarket", "State", "Downtown Crossing", "Chinatown","Park Street", "Back Bay", "Forest Hills"]
+    }
+       start_station = subway[start_line.to_sym].index(start_station)
+         end_station = subway[end_line.to_sym].index(end_station)
+         break_Point_S = subway[start_line.to_sym].index("Park Street")
+         break_Point_E = subway[end_line.to_sym].index("Park Street")
+         if start_line == end_line
+             $result = (start_station - end_station).abs  
+         elsif start_line != end_line
+             steps = (start_station - break_Point_S).abs
+             result = (end_station  - break_Point_E).abs + steps 
+         end
+   end 
+  p stops_between_stations('red', 'Alewife', 'red', 'Alewife') 
+  p stops_between_stations('red', 'Alewife', 'red', 'South Station') 
+ p stops_between_stations('red', 'South Station', 'orange', 'Forest Hills') 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

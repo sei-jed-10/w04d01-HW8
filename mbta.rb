679a8a5d67fa75f76f227ebc7be6c2c45662770e
Mohammedalamry <Mohammedalamry@users.noreply.github.com>
2019-12-15 20:14:16 +0300

    HWComplete

--- mbta.rb
+++ mbta.rb
@@ -1,10 +1,51 @@
 
 # your solution here
-def stops_between_stations initialize(Line1 , Station1, Line2,Station2)
+def stops_between_stations(line1,station1,line2,station2)
 
-    @cust_id = id
-    @cust_name = name
-    @cust_addr = addr
+ 
+    
+   subway={  
+ "Red"=>["South Station",
+        "Kendall",
+        "Central",
+        "Harvard",
+       "Park Street",
+        "Porter",
+      "Davis",
+        "Alewife"], 
+        "Green"=>['Government Center',
+        'Boylston', 
+        'Arlington',
+        'Park Street', 
+        'Copley', 
+        'Hynes', 'Kenmore'],  
+        "Orange"=>[
+        'North Station',
+        'Haymarket',
+        'State',
+        'Downtown Crossing', 
+        'Chinatown',
+        'Park Street',  
+        'Back Bay', 
+        'Forest Hills']  
+       }
+     
+     totall=0
+     if line1==line2 && station1==station2
+     return 0
+     end
+   tot_1= subway[line1].index(station1)-subway[line1].index("Park Street")         
+  
+   totl_2= subway[line2].index(station2)- subway[ line2].index("Park Street") 
+  totall= tot_1+(-totl_2)
+  if totall<0 
+return -(totall)
+
+end
+return totall
+     
+ 
+
 
 
 
@@ -13,14 +54,6 @@ def stops_between_stations initialize(Line1 , Station1, Line2,Station2)
 
 end
 
-
-
-
-
-
-
-
-
-stops_between_stations('Red', 'Alewife', 'Red', 'Park Street')) // 3 stops
-stops_between_stations('Red', 'Alewife', 'Orange', 'Downtown Crossing') // 5 stops
-stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') // 7 stops
+ p stops_between_stations("Red","Alewife","Red","Park Street") 
+ p stops_between_stations('Red','Alewife','Orange',    'Downtown Crossing')  
+  p stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') 
